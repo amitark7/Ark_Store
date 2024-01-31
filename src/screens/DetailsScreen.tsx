4a1645575e027eb73b4fcf6bef22f0cf77bc50ca
amitark7 <amitkumar1600121@gmail.com>
2024-02-01 03:47:31 +0530

    Update DetailsScreen.tsx

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,4 +1,5 @@
 import {
+  FlatList,
   Image,
   ImageBackground,
   ScrollView,
@@ -7,18 +8,18 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import CustomIcon from '../component/CustomIcon';
 
 const DetailsScreen = ({route, navigation}: any) => {
-  const {title, description, rating, price, images, thumbnail} =
-    route.params.item;
-
+  const {title, description, rating, price, images, thumbnail}=route.params.item;
+  const[selectPic,setSelectedPic]=useState(thumbnail)
+  const[pic,setPic]=useState(thumbnail)
   return (
     <ScrollView>
       <View style={styles.Container}>
         <View style={styles.imageContainer}>
-          <Image style={styles.imageBg} source={{uri: thumbnail}} />
+          <Image style={styles.imageBg} source={{uri: pic}} />
           <View style={styles.iconContainer}>
             <TouchableOpacity
               style={styles.icon}
@@ -31,6 +32,26 @@ const DetailsScreen = ({route, navigation}: any) => {
               <CustomIcon name="favorite" size={24} color="#000" />
             </TouchableOpacity>
           </View>
+        </View>
+        <View>
+          <FlatList
+            horizontal
+            data={images}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({item}) => (
+              <TouchableOpacity style={[styles.imageList,{
+                borderColor:selectPic==item?"#F4C724":'#grey'
+              }]} onPress={()=>{
+                setPic(item)
+                setSelectedPic(item)
+              }}>
+                <Image style={styles.image} source={{uri: item}} />
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.titleTxt}>{title}</Text>
         </View>
       </View>
     </ScrollView>
@@ -63,8 +84,28 @@ const styles = StyleSheet.create({
   },
   imageBg: {
     width: '100%',
-    height: 400,
+    height: 300,
     objectFit: 'fill',
     borderRadius: 10,
+  },
+  imageList: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+    borderWidth: 2,
+    borderRadius: 8,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 8,
+  },
+  textContainer: {
+    paddingHorizontal: 10,
+    marginTop: 12,
+  },
+  titleTxt: {
+    fontSize: 26,
+    color: '#000',
+    fontWeight: 'bold',
   },
 });
