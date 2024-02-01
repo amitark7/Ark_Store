3698000d630ffb0c85079cc26d691c6471420192
amitark7 <amitkumar1600121@gmail.com>
2024-02-01 16:49:55 +0530

    Updated

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,18 +1,80 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useContext } from 'react'
-import { contextStore } from '../store/StoreContext'
+import {
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useContext} from 'react';
+import {contextStore} from '../store/StoreContext';
+import HeaderBar from '../component/HeaderBar';
 
 const CartScreen = () => {
-  const {cartList}=useContext(contextStore)
-  console.log(cartList[0].title);
-  
+  const {cartList} = useContext(contextStore);
+
   return (
-    <View>
-      <Text>Cart</Text>
-    </View>
-  )
-}
+    <>
+      <ScrollView style={styles.Container}>
+        <HeaderBar title='Cart'/>
+        <FlatList
+          data={cartList}
+          scrollEnabled={false}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item}) => (
+            <TouchableOpacity style={styles.ListContainer}>
+              <View style={styles.imageContainer}>
+                <Image style={styles.image} source={{uri:item.thumbnail}}/>
+              </View>
+              <View style={styles.innerContainer}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.brand}>Apple</Text>
+                <Text style={styles.price}>${Math.round(
+                    item.price - (item.price * Math.round(item.discountPercentage)) / 100,
+                  )}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+      </ScrollView>
+    </>
+  );
+};
 
-export default CartScreen
+export default CartScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    backgroundColor:'#fff'
+  },
+  ListContainer: {
+    flexDirection:'row',
+    paddingHorizontal:30,
+    marginBottom:10
+  },
+  imageContainer: {},
+  image: {
+    width:140,
+    height:140,
+    borderRadius:8
+  },
+  innerContainer: {
+    marginLeft:16
+  },
+  title: {
+    fontSize:24,
+    fontWeight:'700'
+  },
+  brand: {
+    fontSize:16,
+    marginTop:8
+  },
+  price: {
+    fontSize:22,
+    color:'#000',
+    fontWeight:'bold',
+    marginTop:8
+  },
+});
