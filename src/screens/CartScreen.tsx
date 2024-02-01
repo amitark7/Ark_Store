5367b507e5bec0c49d282330a8e6902d48129e0c
amitark7 <amitkumar1600121@gmail.com>
2024-02-01 17:05:08 +0530

    Update CartScreen.tsx

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -11,6 +11,7 @@ import React, {useContext} from 'react';
 import {contextStore} from '../store/StoreContext';
 import HeaderBar from '../component/HeaderBar';
 import Separator from '../component/Separator';
+import CustomIcon from '../component/CustomIcon';
 
 const CartScreen = () => {
   const {cartList} = useContext(contextStore);
@@ -18,25 +19,32 @@ const CartScreen = () => {
   return (
     <>
       <ScrollView style={styles.Container}>
-        <HeaderBar title='Cart'/>
+        <HeaderBar title="Cart" />
         <FlatList
           data={cartList}
           scrollEnabled={false}
           ItemSeparatorComponent={Separator}
           keyExtractor={(item, index) => index.toString()}
-          renderItem={({item}:any) => (
+          renderItem={({item}: any) => (
             <TouchableOpacity style={styles.ListContainer}>
               <View style={styles.imageContainer}>
-                <Image style={styles.image} source={{uri:item.thumbnail}}/>
+                <Image style={styles.image} source={{uri: item.thumbnail}} />
               </View>
               <View style={styles.innerContainer}>
-                <Text style={styles.title}>{item.title.substring(0,11)}</Text>
+                <Text style={styles.title}>{item.title.substring(0, 11)}</Text>
                 <Text style={styles.brand}>{item.brand}</Text>
-                <Text style={styles.price}>${Math.round(
-                    item.price - (item.price * Math.round(item.discountPercentage)) / 100,
-                  )}</Text>
-                  <Text style={styles.stock}>{item.stock} Items left</Text>
+                <Text style={styles.price}>
+                  $
+                  {Math.round(
+                    item.price -
+                      (item.price * Math.round(item.discountPercentage)) / 100,
+                  )}
+                </Text>
+                <Text style={styles.stock}>{item.stock} Items left</Text>
               </View>
+              <TouchableOpacity>
+                <CustomIcon name='cross' size={24} color='#000'/>
+              </TouchableOpacity>
             </TouchableOpacity>
           )}
         />
@@ -50,42 +58,48 @@ export default CartScreen;
 const styles = StyleSheet.create({
   Container: {
     flex: 1,
-    backgroundColor:'#fff',
-    paddingHorizontal:10
+    backgroundColor: '#fff',
+    paddingHorizontal: 10,
   },
   ListContainer: {
-    flexDirection:'row',
-    paddingHorizontal:20,
-    marginVertical:10
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    marginVertical: 10,
   },
-  imageContainer: {},
+  imageContainer: {
+    
+  },
   image: {
-    width:140,
-    height:140,
-    borderRadius:8
+    width: 140,
+    height: 140,
+    borderRadius: 8,
+    objectFit:'fill'
   },
   innerContainer: {
-    marginLeft:16
+    marginLeft: 16,
   },
   title: {
-    fontSize:24,
-    fontWeight:'700'
+    fontSize: 24,
+    fontWeight: '700',
   },
   brand: {
-    fontSize:16,
-    marginTop:8
+    fontSize: 16,
+    marginTop: 8,
   },
   price: {
-    fontSize:22,
-    color:'#000',
-    fontWeight:'bold',
+    fontSize: 22,
+    color: '#000',
+    fontWeight: 'bold',
+    marginTop: 8,
+  },
+  stock: {
+    color: '#2ecc72',
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    borderWidth: 1,
+    alignSelf: 'center',
+    borderRadius:8,
+    borderColor:'#2ecc72',
     marginTop:8
   },
-  stock:{
-    color:'#2ecc72',
-    paddingVertical:4,
-    paddingHorizontal:8,
-    borderWidth:1,
-    alignSelf:'center'
-  }
 });
