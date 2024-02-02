4013066792168397ddf12bbfe185062b2be2321e
amitark7 <amitkumar1600121@gmail.com>
2024-02-02 16:10:56 +0530

    Update PaymentScreen.tsx

--- src/screens/PaymentScreen.tsx
+++ src/screens/PaymentScreen.tsx
@@ -24,6 +24,28 @@ const PaymentScreen = ({navigation}: any) => {
       <View style={styles.midContainer}>
         <View style={styles.creditCardContainer}>
           <Text style={styles.cardTitle}>Credit Card</Text>
+          <View style={styles.cardContainer}>
+            <View style={styles.cardTopContainer}>
+              <CustomIcon name="" size={28} color="#fff" />
+              <CustomIcon name="" size={28} color="#fff" />
+            </View>
+            <View style={styles.numberContainer}>
+              <Text style={styles.number}>3703</Text>
+              <Text style={styles.number}>5177</Text>
+              <Text style={styles.number}>7777</Text>
+              <Text style={styles.number}>3703</Text>
+            </View>
+            <View style={styles.holderContainer}>
+              <View>
+                <Text style={styles.title}>Card Holder Name</Text>
+                <Text style={styles.subTitle}>Amit Kumar</Text>
+              </View>
+              <View>
+                <Text style={styles.title}>Expiry Date</Text>
+                <Text style={styles.subTitle}>02/28</Text>
+              </View>
+            </View>
+          </View>
         </View>
       </View>
     </ScrollView>
@@ -55,6 +77,12 @@ const styles = StyleSheet.create({
     top: '30%',
   },
   midContainer: {},
-  creditCardContainer:{},
-  cardTitle:{}
+  creditCardContainer: {},
+  cardTitle: {},
+  cardContainer: {},
+  cardTopContainer: {},
+  numberContainer: {},
+  number: {},
+  holderContainer: {},
+  title:{}
 });
