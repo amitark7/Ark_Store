d0c41fd750ac732b92d146733eee1f68e0b322ab
amitark7 <amitkumar1600121@gmail.com>
2024-02-05 15:46:45 +0530

    Update OrderHistoryList.tsx

--- src/screens/OrderHistoryList.tsx
+++ src/screens/OrderHistoryList.tsx
@@ -6,7 +6,7 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import HeaderBar from '../component/HeaderBar';
 import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
 import {itemStore} from '../store/itemStore';
@@ -15,79 +15,100 @@ import LottieView from 'lottie-react-native';
 const OrderHistoryList = ({navigation}: any) => {
   const TabHeight = useBottomTabBarHeight();
   const OrderHistoryList = itemStore((state: any) => state.OrderHistoryList);
+  const [showAnimation, setShowanimatison] = useState(true);
 
   return (
-    <View style={styles.ScreenContainer}>
-      <ScrollView
-        style={[styles.ScrollViewFlex]}
-        showsVerticalScrollIndicator={false}>
-        <HeaderBar title="Order History" />
-        {OrderHistoryList.length == 0 ? (
-          <View style={styles.EmptyContainer}>
-            <LottieView style={styles.cartImage} source={require('../assets/lotti/cart.json')} autoPlay loop/>
-            <Text style={styles.emptyTxt}>No Order History</Text>
-          </View>
-        ) : (
-          OrderHistoryList.map((item: any, index: any) => (
-            <View style={styles.OrderListContainer} key={index}>
-              <View style={styles.TopContainer}>
-                <View>
-                  <Text style={styles.subTitle}>Order Time</Text>
-                  <Text style={styles.titleTxt}>{item.OrderDate}</Text>
-                </View>
-                <View>
-                  <Text style={styles.subTitle}>Total Amount</Text>
-                  <Text style={styles.PriceTxt}>${item.CartListPrice}</Text>
-                </View>
-              </View>
-              <View>
-                {item.CartList.map((item: any, index: any) => (
-                  <TouchableOpacity
-                    key={index}
-                    style={styles.ListContainer}
-                    onPress={() => {
-                      navigation.push('Details', {item});
-                    }}>
-                    <View>
-                      <Image
-                        style={styles.image}
-                        source={{uri: item.thumbnail}}
-                      />
-                    </View>
-                    <View style={styles.innerContainer}>
-                      <Text style={styles.title}>{item.title}</Text>
-                      <Text style={styles.brand}>{item.brand}</Text>
-                      <Text style={styles.price}>
-                        $
-                        {Math.round(
-                          item.price -
-                            (item.price * Math.round(item.discountPercentage)) /
-                              100,
-                        )}
-                      </Text>
-                    </View>
-                  </TouchableOpacity>
-                ))}
-              </View>
-            </View>
-          ))
-        )}
-      </ScrollView>
-      {OrderHistoryList.length > 0 ? (
-        <View style={[styles.bottomContainer, {marginBottom: TabHeight}]}>
-          <TouchableOpacity
-            style={styles.buyBtn}
-            onPress={() => {
-              // OrderListFromCartList();
-              // navigation.navigate('History');
-            }}>
-            <Text style={styles.BuyTxt}>Download</Text>
-          </TouchableOpacity>
+    <>
+      {showAnimation ? (
+        <View style={styles.LottiConatiner}>
+          <LottieView
+            style={styles.Lotti}
+            source={require('../assets/lotti/successful.json')}
+            autoPlay
+            loop
+          />
         </View>
       ) : (
         <></>
       )}
-    </View>
+      <View style={styles.ScreenContainer}>
+        <ScrollView
+          style={[styles.ScrollViewFlex]}
+          showsVerticalScrollIndicator={false}>
+          <HeaderBar title="Order History" />
+          {OrderHistoryList.length == 0 ? (
+            <View style={styles.EmptyContainer}>
+              <LottieView
+                style={styles.cartImage}
+                source={require('../assets/lotti/cart.json')}
+                autoPlay
+                loop
+              />
+              <Text style={styles.emptyTxt}>No Order History</Text>
+            </View>
+          ) : (
+            OrderHistoryList.map((item: any, index: any) => (
+              <View style={styles.OrderListContainer} key={index}>
+                <View style={styles.TopContainer}>
+                  <View>
+                    <Text style={styles.subTitle}>Order Time</Text>
+                    <Text style={styles.titleTxt}>{item.OrderDate}</Text>
+                  </View>
+                  <View>
+                    <Text style={styles.subTitle}>Total Amount</Text>
+                    <Text style={styles.PriceTxt}>${item.CartListPrice}</Text>
+                  </View>
+                </View>
+                <View>
+                  {item.CartList.map((item: any, index: any) => (
+                    <TouchableOpacity
+                      key={index}
+                      style={styles.ListContainer}
+                      onPress={() => {
+                        navigation.push('Details', {item});
+                      }}>
+                      <View>
+                        <Image
+                          style={styles.image}
+                          source={{uri: item.thumbnail}}
+                        />
+                      </View>
+                      <View style={styles.innerContainer}>
+                        <Text style={styles.title}>{item.title}</Text>
+                        <Text style={styles.brand}>{item.brand}</Text>
+                        <Text style={styles.price}>
+                          $
+                          {Math.round(
+                            item.price -
+                              (item.price *
+                                Math.round(item.discountPercentage)) /
+                                100,
+                          )}
+                        </Text>
+                      </View>
+                    </TouchableOpacity>
+                  ))}
+                </View>
+              </View>
+            ))
+          )}
+        </ScrollView>
+        {OrderHistoryList.length > 0 ? (
+          <View style={[styles.bottomContainer, {marginBottom: TabHeight}]}>
+            <TouchableOpacity
+              style={styles.buyBtn}
+              onPress={() => {
+                // OrderListFromCartList();
+                // navigation.navigate('History');
+              }}>
+              <Text style={styles.BuyTxt}>Download</Text>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <></>
+        )}
+      </View>
+    </>
   );
 };
 
@@ -191,5 +212,20 @@ const styles = StyleSheet.create({
   cartImage: {
     width: 200,
     height: 200,
+  },
+  LottiConatiner: {
+    position: 'absolute',
+    flex: 1,
+    top:0,
+    left:0,
+    right:0
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#0000000000',
+  },
+  Lotti: {
+   flex:1,
+   height:200,
+   width:200
   },
 });
