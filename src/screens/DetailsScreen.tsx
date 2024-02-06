dfd290cb355451f1fabc12821ffc2def37adba47
amitark7 <amitkumar1600121@gmail.com>
2024-02-06 19:19:22 +0530

    Update DetailsScreen.tsx

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -19,12 +19,6 @@ const DetailsScreen = ({route, navigation}: any) => {
 
   const [selectPic, setSelectedPic] = useState(images[0]);
   const [pic, setPic] = useState(images[0]);
-
-  const handleCart = async (item: any) => {
-    // addToCart(item);
-    // const newCart = [...cartList, item];
-    // await AsyncStorage.setItem('cart', JSON.stringify(newCart));
-  };
   return (
     <>
       <ScrollView>
